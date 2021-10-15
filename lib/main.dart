93e42184cf63c958d8d1db382849086229b79a26
leve0001 <33024771+leve0001@users.noreply.github.com>
2021-10-15 11:39:02 +0200

    + added stay logged in +addTeam

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:master_projekt/Screens/login.dart';
@@ -10,6 +13,8 @@ import 'package:master_projekt/Screens/tasks.dart';
 import 'package:master_projekt/Screens/welcomeScreen.dart';
 import 'package:master_projekt/Screens/register.dart';
 import 'package:master_projekt/Screens/passwortVergessen.dart';
+import 'package:master_projekt/Screens/teams.dart';
+import 'package:master_projekt/Screens/addTeam.dart';
 
 
 void main() async {
@@ -18,8 +23,27 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  late StreamSubscription<User?> user;
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+        print(FirebaseAuth.instance.currentUser);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,13 +60,16 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute:
+      FirebaseAuth.instance.currentUser == null ? '/' : '/homepage',
       routes: {
         '/': (context) => const Welcome(),
         '/login': (context) => const Login(),
         '/register': (context) => const Register(),
         '/password': (context) => const Password(),
         '/homepage': (context) => const Homepage(),
+        '/teams': (context) => const Teams(),
+        '/addTeam': (context) => const addTeam(),
         '/tasks': (context) => const Tasks(),
         '/challenges': (context) => const Challenges(),
         '/leaderboards': (context) => const Leaderboards(),
