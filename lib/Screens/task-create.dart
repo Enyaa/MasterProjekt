f3c3ff793425b6686a446d550e59a3930b239081
Enyaa <lenamarnolds@gmail.com>
2021-10-27 16:23:12 +0200

    todo: add updating view

--- lib/Screens/task-create.dart
+++ lib/Screens/task-create.dart
@@ -53,9 +53,7 @@ class _TaskCreateState extends State<TaskCreate> {
         print('No subtask');
       } else {
         print(subTasks.toString());
-        setState(() {
-          subTasks.add(task);
-        });
+        subTasks.add(task);
         print(subTasks.toString());
       }
     }
@@ -82,108 +80,154 @@ class _TaskCreateState extends State<TaskCreate> {
                     ],
                   ));
           return willLeave;
-        },child: Scaffold(
-        appBar: AppBar(title: const Text('Aufgaben')),
-        drawer: MyDrawer(),
-        body: Form(
-          key: _formKey,
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            child: ListView(
-              children: [
-              TextFormField(
-                controller: titleController,
-                maxLength: 20,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Titel',
-                  labelText: 'Titel'
-                ),
-                validator: (value) {
-                  if(value == null || value.isEmpty) {
-                    return 'Bitte geben Sie einen Titel an.';
-                  }
-                  return null;
-                },
-              ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                ),
-              TextFormField(
-                controller: descriptionController,
-                maxLines: 5,
-                maxLength: 200,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Beschreibung',
-                    labelText: 'Beschreibung'
-                ),
-                validator: (value) {
-                  if(value == null || value.isEmpty) {
-                    return 'Bitte geben Sie eine Beschreibung an.';
-                  }
-                  return null;
-                },
-              ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                ),
-                Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+        },
+        child: Scaffold(
+            appBar: AppBar(title: const Text('Aufgaben')),
+            drawer: MyDrawer(),
+            body: Form(
+                key: _formKey,
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  child: ListView(
                     children: [
-                      Container(
-                        width: 250,
-                        child: TextFormField(
-                          controller: subTaskController,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Hier Teilaufgaben hinzufügen',
-                              labelText: 'Teilaufgabe',
+                      TextFormField(
+                        controller: titleController,
+                        maxLength: 20,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Titel',
+                            labelText: 'Titel'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Bitte geben Sie einen Titel an.';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                      ),
+                      TextFormField(
+                        controller: descriptionController,
+                        maxLines: 5,
+                        maxLength: 200,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Beschreibung',
+                            labelText: 'Beschreibung'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Bitte geben Sie eine Beschreibung an.';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 250,
+                              child: TextFormField(
+                                controller: subTaskController,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Hier Teilaufgaben hinzufügen',
+                                  labelText: 'Teilaufgabe',
+                                ),
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.all(10)),
+                            ElevatedButton(
+                                onPressed: () {
+                                  addSubTask(subTaskController.text);
+                                  subTaskController.text = '';
+                                },
+                                child: Icon(Icons.add),
+                                style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder()))
+                          ]),
+                      SizedBox(
+                        height: 50,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: subTasks.length,
+                          itemBuilder: (context, index) => GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                subTasks.remove(subTasks[index]);
+                              });
+                            },
+                            child: Container(
+                                margin: EdgeInsets.all(7.0),
+                                height: 10,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5),
+                                          spreadRadius: 0.5,
+                                          blurRadius: 0.5,
+                                          offset: Offset(1, 1))
+                                    ]),
+                                child: Center(
+                                  child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 4.0),
+                                            child: Icon(
+                                              Icons.remove_circle_outline,
+                                              size: 17,
+                                            ),
+                                          ),
+                                          Text(subTasks[index]),
+                                        ],
+                                      )),
+                                )),
                           ),
                         ),
                       ),
-                  Padding(padding: EdgeInsets.all(10)),
-                  ElevatedButton(onPressed: () {
-                    addSubTask(subTaskController.text);
-                    subTaskController.text = '';
-                    },
-                      child: Icon(Icons.add), style: ElevatedButton.styleFrom(shape: CircleBorder()))
-                ]),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                ),
-              TextFormField(
-                controller: xpController,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Punkte',
-                    labelText: 'Punkte'
-                ),
-                validator: (value) {
-                  if(value == null || value.isEmpty) {
-                    return 'Bitte geben Sie eine Punktzahl an.';
-                  }
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if(_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      addTask(titleController.text, descriptionController.text, xpController.text);
-                      Navigator.pushReplacementNamed(context, '/tasks');
-                    }
-                  },
-                  child: Text('Speichern')
-              )
-            ],
-          ),
-        )
-    ),
-        bottomNavigationBar: NavigationBar(0)
-    ));
-
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                      ),
+                      TextFormField(
+                        controller: xpController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Punkte',
+                            labelText: 'Punkte'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Bitte geben Sie eine Punktzahl an.';
+                          }
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                              addTask(
+                                  titleController.text,
+                                  descriptionController.text,
+                                  xpController.text);
+                              Navigator.pushReplacementNamed(context, '/tasks');
+                            }
+                          },
+                          child: Text('Speichern'))
+                    ],
+                  ),
+                )),
+            bottomNavigationBar: NavigationBar(0)));
   }
 }
