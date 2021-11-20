80ec5ece740a10c6ca326c63af63d757eb152d21
Enyaa <lenamarnolds@gmail.com>
2021-11-20 16:59:53 +0100

    feat: added MyWillPopScope -> add to all screens with adjusted child and text

---
+++ lib/navigation/willpopscope.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyWillPopScope extends StatefulWidget {
+  const MyWillPopScope({Key? key, required this.child, required this.text})
+      : super(key: key);
+
+  final Widget child;
+  final String text;
+
+  @override
+  _MyWillPopScopeState createState() => _MyWillPopScopeState();
+}
+
+class _MyWillPopScopeState extends State<MyWillPopScope> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        bool willLeave = false;
+        // show the confirm dialog
+        await showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              backgroundColor: Color(0xff353535),
+                  title: Text(widget.text),
+                  actions: [
+                    Container(
+                        width: 100,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              // 10% of the width, so there are ten blinds.
+                              colors: <Color>[
+                                Color(0xffE53147),
+                                Color(0xffFB9C26)
+                              ],
+                              // red to yellow
+                              tileMode: TileMode
+                                  .repeated, // repeats the gradient over the canvas
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.transparent),
+                                shadowColor: MaterialStateProperty.all(
+                                    Colors.transparent)),
+                            onPressed: () {
+                              willLeave = false;
+                              SystemNavigator.pop(); // might not work with iOS
+                            },
+                            child: Text('Ja'))),
+                    Container(
+                        height: 50,
+                        width: 100,
+                        child: OutlinedButton(
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.transparent),
+                                shadowColor: MaterialStateProperty.all(
+                                    Colors.transparent)),
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: Text('Nein')))
+                  ],
+                ));
+        return willLeave;
+      },
+      child: widget.child,
+    );
+  }
+}
