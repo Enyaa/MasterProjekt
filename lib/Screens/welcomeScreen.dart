c5d7b82cca226666d43f5c044fc9374eff2ca3bd
leve0001 <33024771+leve0001@users.noreply.github.com>
2021-12-04 12:32:00 +0100

    ~changed welcome to scrollview

--- lib/Screens/welcomeScreen.dart
+++ lib/Screens/welcomeScreen.dart
@@ -13,74 +13,76 @@ class Welcome extends StatelessWidget {
         close: true,
         child: Scaffold(
         body: Center(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(padding: EdgeInsets.all(20)),
-                  Image.asset('lib/Graphics/rettich.png', height: 350, width: 350,),
-                  ShaderMask(
-                    shaderCallback: (Rect bounds){
-                      return gradient.createShader(Offset.zero & bounds.size);
-                    },
-                    child: Center(
-                      child: Text("TeamRad", style: TextStyle(
-                          fontSize: 60,
-                          fontWeight: FontWeight.bold,
-                          height: 2)),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(padding: EdgeInsets.all(20)),
+                    Image.asset('lib/Graphics/rettich.png', height: 350, width: 350,),
+                    ShaderMask(
+                      shaderCallback: (Rect bounds){
+                        return gradient.createShader(Offset.zero & bounds.size);
+                      },
+                      child: Center(
+                        child: Text("TeamRad", style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold,
+                            height: 2)),
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: 370,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                          // 10% of the width, so there are ten blinds.
-                          colors: <Color>[
-                            Color(0xffE53147),
-                            Color(0xffFB9C26)
-                          ],
-                          // red to yellow
-                          tileMode: TileMode
-                              .repeated, // repeats the gradient over the canvas
-                        ),
-                        borderRadius:
-                        BorderRadius.all(Radius.circular(50))),
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Colors.transparent),
-                            shadowColor: MaterialStateProperty.all(
-                                Colors.transparent)),
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/login');
-                        },
-                        child: Text("Login")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Container(
+                    Container(
+                      width: 370,
                       height: 50,
-                      width: 300,
-                      child: OutlinedButton(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            // 10% of the width, so there are ten blinds.
+                            colors: <Color>[
+                              Color(0xffE53147),
+                              Color(0xffFB9C26)
+                            ],
+                            // red to yellow
+                            tileMode: TileMode
+                                .repeated, // repeats the gradient over the canvas
+                          ),
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(50))),
+                      child: ElevatedButton(
                           style: ButtonStyle(
                               backgroundColor: MaterialStateProperty.all(
                                   Colors.transparent),
                               shadowColor: MaterialStateProperty.all(
                                   Colors.transparent)),
                           onPressed: () {
-                            Navigator.pushReplacementNamed(context, '/register');
+                            Navigator.pushReplacementNamed(context, '/login');
                           },
-                          child: Text("Registrieren")),
+                          child: Text("Login")),
                     ),
-                  ),
-                  TextButton(onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/password');
-                  }, child: Text("Passwort vergessen?", style: TextStyle(color: Colors.white)),)
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Container(
+                        height: 50,
+                        width: 300,
+                        child: OutlinedButton(
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.transparent),
+                                shadowColor: MaterialStateProperty.all(
+                                    Colors.transparent)),
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, '/register');
+                            },
+                            child: Text("Registrieren")),
+                      ),
+                    ),
+                    TextButton(onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/password');
+                    }, child: Text("Passwort vergessen?", style: TextStyle(color: Colors.white)),)
+                  ],
+                ),
               ),
             )
         )));
